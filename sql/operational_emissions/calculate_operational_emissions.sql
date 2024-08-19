961efd4a7e963742e1c472d9152496419fdff018
TanyaTsui <tanya.tsui2010@gmail.com>
2024-08-19 13:11:55 +0200

    completed OperationalEmissionsPipeline

---
+++ sql/operational_emissions/calculate_operational_emissions.sql
@@ -0,0 +1,20 @@
+ALTER TABLE cbs_map_all
+DROP COLUMN IF EXISTS emissions_kg_gas,
+DROP COLUMN IF EXISTS emissions_kg_electricity,
+DROP COLUMN IF EXISTS emissions_kg_total,
+DROP COLUMN IF EXISTS emissions_kg_pp;
+
+ALTER TABLE cbs_map_all
+ADD COLUMN emissions_kg_gas NUMERIC,
+ADD COLUMN emissions_kg_electricity NUMERIC,
+ADD COLUMN emissions_kg_total NUMERIC,
+ADD COLUMN emissions_kg_pp NUMERIC;
+
+UPDATE cbs_map_all
+SET emissions_kg_gas = g_gas_tot * 1.9,
+    emissions_kg_electricity = g_elek_tot * 0.45,
+    emissions_kg_total = (g_gas_tot * 1.9) + (g_elek_tot * 0.45),
+    emissions_kg_pp = CASE 
+                        WHEN aant_inw <= 0 THEN NULL
+                        ELSE ((g_gas_tot * 1.9) + (g_elek_tot * 0.45)) / aant_inw 
+                      END;
